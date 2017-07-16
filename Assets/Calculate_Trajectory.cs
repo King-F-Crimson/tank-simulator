8011295ce589f981ceb2d3f9b7da7668f9e6c75d
tom <11286187+Regular-Tom@users.noreply.github.com>
2017-07-16 17:56:46 +1200

    Initial commit

---
+++ Assets/Calculate_Trajectory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculate_Trajectory : MonoBehaviour {
+
+	public GameObject shooter;
+	public float timeStep = 0.1f;
+	public int points = 100;
+
+	private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.positionCount = points;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CalculateTrajectory();
+	}
+
+	void CalculateTrajectory() {
+		Vector3 position; Quaternion rotation; float v_velocity; float h_velocity;
+		shooter.GetComponent<Shoot>().GetProjectileInstantiationData(out position, out rotation, out v_velocity, out h_velocity);
+
+		for (int i = 0; i < points; i++) {
+			lineRenderer.SetPosition(i, CalculateProjectilePosition(i * timeStep, position, rotation, v_velocity, h_velocity));
+		}
+	}
+
+	Vector3 CalculateProjectilePosition(float timeElapsed, Vector3 initialPosition, Quaternion rotation, float vVelocity, float hVelocity) {
+		float gravity = Physics.gravity[1];
+
+		float verticalMovement = gravity * (float)Math.Pow((double)timeElapsed, 2.0) / 2 + vVelocity * timeElapsed;
+		float horizontalMovement = hVelocity * timeElapsed;
+
+		return new Vector3(horizontalMovement, verticalMovement, 0) + initialPosition;
+	}
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject projectile;
+	public Slider verticalVelocitySlider;
+	public Slider horizontalVelocitySlider;
+
+	public Vector3 muzzlePosition;
+
+	private float verticalVelocity;
+	private float horizontalVelocity;
+
+	// Use this for initialization.
+	void Start () {
+		verticalVelocitySlider.onValueChanged.AddListener(delegate {ChangeVelocity(); });
+		horizontalVelocitySlider.onValueChanged.AddListener(delegate {ChangeVelocity(); });
+
+		verticalVelocity = verticalVelocitySlider.value;
+		horizontalVelocity = horizontalVelocitySlider.value;
+	}
+	
+	// Update is called once per frame.
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftShift)) {
+			GameObject clone = Instantiate(projectile, transform.position + muzzlePosition, transform.rotation);
+			Rigidbody rb = clone.GetComponent<Rigidbody>();
+
+			rb.velocity = new Vector3(horizontalVelocity, verticalVelocity, 0);
+		}
+	}
+
+	void ChangeVelocity() {
+		verticalVelocity = verticalVelocitySlider.value;
+		horizontalVelocity = horizontalVelocitySlider.value;
+	}
+
+	// Returns the initial coordinates and velocities.
+	public void GetProjectileInstantiationData(out Vector3 position,
+		out Quaternion rotation,
+		out float verticalVelocityInput,
+		out float horizontalVelocityInput) {
+		position = transform.position + muzzlePosition;
+		rotation = transform.rotation;
+		verticalVelocityInput = verticalVelocity;
+		horizontalVelocityInput = horizontalVelocity;
+	}
+}
