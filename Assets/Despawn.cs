9c714f5216fab3d1900fcf65679325daa836f05c
tom <11286187+Regular-Tom@users.noreply.github.com>
2017-07-26 12:25:12 +1200

    Add new tank model and cannonball texture.

---
+++ Assets/Despawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despawn : MonoBehaviour {
+	public float life = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (life > 0) {
+			life -= Time.deltaTime;
+		}
+		else {
+			Destroy(this.gameObject);
+		}
+	}
+}
