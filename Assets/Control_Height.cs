065771159b615ce34f87ec95add7d0e9aecc6b8d
tom <11286187+Regular-Tom@users.noreply.github.com>
2017-08-30 11:35:42 +1200

    Add slider to control the height of the tank based on the muzzle height.

---
+++ Assets/Control_Height.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Control_Height : MonoBehaviour {
+
+    public float initialHeight = 2.1f;
+    public Slider heightSlider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (0, heightSlider.value - initialHeight, 0);
+	}
+}
