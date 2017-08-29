7a0ff2d9ea57482532ed6de6600c0b66b249b8fe
tom <11286187+Regular-Tom@users.noreply.github.com>
2017-08-30 11:10:34 +1200

    Add pause controller.

---
+++ Assets/Pause_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Controller : MonoBehaviour {
+    public bool IsPaused = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P)) {
+            if (IsPaused) {
+                Time.timeScale = 1;
+                IsPaused = false;
+            }
+            else {
+                Time.timeScale = 0;
+                IsPaused = true;
+            }
+        }
+	}
+}
